4b4fe823b7c58d5c1919841878e8c8dc2c08ea3d
Den <den.dovgan@gmail.com>
2023-03-16 22:48:29 +0200

    update `job`

--- src/job.rs
+++ src/job.rs
@@ -1,24 +1,16 @@
 use std::thread;
 
 pub struct Job {
-    job_id: u32,
     execution_closure: Box<dyn Fn() -> () + Send + 'static + Sync>,
 }
 
 impl Job {
-    pub fn new(
-        job_id: u32,
-        execution_closure: Box<dyn Fn() -> () + Send + 'static + Sync>,
-    ) -> Self {
-        Self {
-            job_id,
-            execution_closure,
-        }
+    pub fn new(execution_closure: Box<dyn Fn() -> () + Send + 'static + Sync>) -> Self {
+        Self { execution_closure }
     }
 
     pub fn execute(&self) {
         let current_thread = thread::current();
-        println!("executing job #{} on {:?}", self.job_id, current_thread);
         (self.execution_closure)();
     }
 }
